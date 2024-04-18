e79c5af8f4a2f2692be77c71be20bbaaa1467ecb
sanjudesai2929 <sanjudesai2929@gmail.com>
2024-04-18 18:08:27 +0530

    first commit

--- server.js
+++ server.js
@@ -8,53 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const twilio = require('twilio');
 require('dotenv').config();
-// Your Twilio Account SID and Auth Token from twilio.com/console
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
 
-// Create a Twilio client
-const client = new twilio(accountSid, authToken);
-
-// Your Twilio phone number
-const twilioPhoneNumber = '+14435507227';
-
-// The recipient's phone number
-const toPhoneNumber = '+91 98799 86027'; // replace with the actual phone number
-
-// The message you want to send
-const messageBody = 'Hello, this is a test message from Twilio!';
-const statusCallbackUrl = 'http://localhost:4000/twilio/status'; // replace with your server's URL
-
-// Send the message
-
-// Endpoint to handle Twilio status callbacks
-app.post('/twilio/status', (req, res) => {
-    console.log("hh");
-  const messageSid = req.body.MessageSid;
-  const messageStatus = req.body.MessageStatus;
-
-  console.log(`Received status update for message SID ${messageSid}: ${messageStatus}`);
-
-  // You can handle the status update here as needed
-
-  res.status(200).end();
-});
-app.get("/send",(req,res)=>{
-  client.messages
-  .create({
-    body: messageBody,
-    from: twilioPhoneNumber,
-    to: toPhoneNumber,
-    statusCallback: statusCallbackUrl
-  })
-  .then((message) => res.send(message))
-  .catch((error) => console.error(`Error sending message: ${error.message}`));
-})
-app.get('/', (req, res) => {
-
-  
-    res.status(200).end("rf");
-  });
 
 // Start the server
 app.listen(port, () => {
